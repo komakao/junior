14418a498ca841932f5b93f307259ded2392b2a2
t005 <t005@mail.nksh.tp.edu.tw>
2018-03-01 22:42:02 -0500

    update

--- teacher/urls.py
+++ teacher/urls.py
@@ -25,12 +25,11 @@ urlpatterns = [
     url(r'^announce/download/(?P<messagefile_id>\d+)/$', views.announce_download),		
     #作業
     url(r'^work/(?P<classroom_id>\d+)/$', views.work),    
+    url(r'^work/group/(?P<classroom_id>\d+)/$', views.work_group),		
     url(r'^assistant/(?P<classroom_id>\d+)/(?P<user_id>\d+)/(?P<lesson>\d+)/(?P<index>\d+)/$', views.assistant), 
     url(r'^assistant_cancle/(?P<classroom_id>\d+)/(?P<user_id>\d+)/(?P<lesson>\d+)/(?P<index>\d+)$', views.assistant_cancle),  
     url(r'^score_peer/(?P<lesson>\d+)/(?P<index>\d+)/(?P<classroom_id>\d+)/(?P<group>\d+)/$', views.score_peer),     
     url(r'^scoring/(?P<classroom_id>[^/]+)/(?P<user_id>\d+)/(?P<lesson>\d+)/(?P<index>\d+)/$', views.scoring),     
     url(r'^score/(?P<classroom_id>\d+)/(?P<lesson>\d+)/(?P<index>\d+)/$', views.score),   
-    #url(r'^score/group/(?P<lesson>\d+)/(?P<index>\d+)/(?P<classroom_id>\d+)/$', views.score_group),   
-    url(r'^work/group/(?P<classroom_id>\d+)/$', views.work_group),	
-   		
+    #url(r'^score/group/(?P<lesson>\d+)/(?P<index>\d+)/(?P<classroom_id>\d+)/$', views.score_group),    		
 ]
