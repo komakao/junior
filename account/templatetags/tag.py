655b8d532da0b323e88ce80aa7c82f97d81a8166
t005 <t005@mail.nksh.tp.edu.tw>
2018-03-02 00:10:49 -0500

    update

--- account/templatetags/tag.py
+++ account/templatetags/tag.py
@@ -40,7 +40,7 @@ def classroom(user_id):
     if user_id > 0 :
         enrolls = Enroll.objects.filter(student_id=user_id).order_by("-id")
         classroom_names = ""
-        for enroll in enrolls:
+        for enroll in enrolls[0:5]:
             classroom = Classroom.objects.get(id=enroll.classroom_id)
             classroom_names += classroom.name + "| "
         return classroom_names
